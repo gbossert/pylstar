efb392fccf672f68d97029ef124c782e17699c8a
Olivier Levillain <olivier.levillain@picty.org>
2022-06-24 14:13:00 +0200

    Clean up BDistMethod a little.

--- src/pylstar/eqtests/BDistMethod.py
+++ src/pylstar/eqtests/BDistMethod.py
@@ -194,6 +194,7 @@ class BDistMethod:
         self.knowledge_base = knowledge_base
         self.input_letters = input_letters
         self.bdist = bdist
+        self.representatives: dict[str, Word] = {}
         self.suffix_list: List[Word] = []
         for i in range(1, self.bdist + 1):
             for suffix_tuple in product(self.input_letters, repeat=i):
@@ -205,12 +206,12 @@ class BDistMethod:
 
         self._logger.info("Starting the BDistMethod Algorithm to search for a counter-example")
 
-        representatives: dict[str, Word] = self.get_representatives(hypothesis)
+        self.__compute_representatives(hypothesis)
 
         for q in hypothesis.get_states():
             for letter in self.input_letters:
                 word = Word([letter])
-                w_i = representatives[q.name] + word
+                w_i = self.representatives[q.name] + word
                 out = hypothesis.play_word(word, q)
 
                 query = OutputQuery(w_i)
@@ -220,27 +221,24 @@ class BDistMethod:
                     return query
 
                 q_prime = out[1][-1]  # The last state visited by playing the word
-                w_i_prime = representatives[q_prime.name]
+                w_i_prime = self.representatives[q_prime.name]
                 if w_i == w_i_prime:
                     continue
 
-                suffix, query_i, query_i_prime = self.__check_equivalence(
-                    w_i, w_i_prime
-                )
+                suffix, query_i, query_i_prime = self.__check_equivalence(w_i, w_i_prime)
                 if suffix:
                     expected_output_word = hypothesis.play_word(w_i + suffix)[0].letters
                     if expected_output_word != query_i.output_word.letters:
                         query = query_i
                     else:
-                        expected_output_word = hypothesis.play_word(w_i_prime + suffix)[
-                            0
-                        ].letters
+                        expected_output_word = hypothesis.play_word(w_i_prime + suffix)[0].letters
                         query = query_i_prime
                     self._logger.info("Found a counter-example : input: '{}', expected: '{}', observed: '{}'".format(query.input_word, expected_output_word, query.output_word))
                     return query
+
         return None
 
-    def get_representatives(self, automaton: Automata):
+    def __compute_representatives(self, automaton: Automata):
         """Return a dictionary that, for each state, gives a word of the
            shortest length possible to reach this state.
 
@@ -248,28 +246,21 @@ class BDistMethod:
 
         nb_states = len(automaton.get_states())
         currentState = automaton.initial_state
-        representatives = {currentState.name: Word([])}
-        to_analyze = [
-            (
-                t.output_state,
-                representatives[currentState.name] + Word([t.input_letter]),
-            )
+        self.representatives = {currentState.name: Word([])}
+        to_analyze = self.__add_candidates_to_analyze(currentState)
+
+        while to_analyze and len(self.representatives) < nb_states:
+            (currentState, in_word) = to_analyze.pop()
+            if currentState.name not in self.representatives.keys():
+                self.representatives[currentState.name] = in_word
+                to_analyze = to_analyze + self.__add_candidates_to_analyze(currentState)
+
+    def __add_candidates_to_analyze(self, currentState):
+        return [
+            (t.output_state, self.representatives[currentState.name] + Word([t.input_letter]))
             for t in currentState.transitions
             if t.output_state.name != currentState.name
         ]
-        while len(to_analyze) > 0 and len(representatives) < nb_states:
-            (currentState, in_word) = to_analyze.pop()
-            if currentState.name not in representatives.keys():
-                representatives[currentState.name] = in_word
-                to_analyze = to_analyze + [
-                    (
-                        t.output_state,
-                        representatives[currentState.name] + Word([t.input_letter]),
-                    )
-                    for t in currentState.transitions
-                    if t.output_state.name not in representatives.keys()
-                ]
-        return representatives
 
     def __check_equivalence(self, w_i: Word, w_i_prime: Word):
         for suffix in self.suffix_list:
@@ -277,9 +268,6 @@ class BDistMethod:
             query_i_prime = OutputQuery(w_i_prime + suffix)
             self.knowledge_base.resolve_query(query_i)
             self.knowledge_base.resolve_query(query_i_prime)
-            if (
-                query_i.output_word.last_letter()
-                != query_i_prime.output_word.last_letter()
-            ):
+            if (query_i.output_word.last_letter() != query_i_prime.output_word.last_letter()):
                 return suffix, query_i, query_i_prime
         return None, None, None
